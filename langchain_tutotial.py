0c5b393c5b12d86db2f8676ebad9633f62e3deda
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-04-22 21:22:06 -0700

    agent test + duckduckgo as search

--- langchain_tutotial.py
+++ langchain_tutotial.py
@@ -1,6 +1,3 @@
-# import os
-# os.environ["OPENAI_API_KEY"] = "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"
-
 from langchain.llms import OpenAI
 
 ## TEST: Simple Call
@@ -32,20 +29,40 @@ from langchain.llms import OpenAI
 
 ## TEST: Mixed
 # Test Result No Memory.
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
+# from langchain.chains import LLMChain
+# from langchain.prompts import PromptTemplate
 
-# OpenAI has Request Rate Limitation.
-llm = OpenAI(temperature=0.9, model_name="gpt-3.5-turbo")
-prompt = PromptTemplate(
-    input_variables=["cmd"],
-    template="{cmd}",
-)
-chain = LLMChain(llm=llm, prompt=prompt)
+# # OpenAI has Request Rate Limitation.
+# llm = OpenAI(temperature=0.9, model_name="gpt-3.5-turbo")
+# prompt = PromptTemplate(
+#     input_variables=["cmd"],
+#     template="{cmd}",
+# )
+# chain = LLMChain(llm=llm, prompt=prompt)
 
-next_command = input("> ")
-while (next_command != "stop"):
-	print(chain.run(next_command).strip())
-	next_command = input("> ")
-print("Conv Over.")
+# next_command = input("> ")
+# while (next_command != "stop"):
+# 	print(chain.run(next_command).strip())
+# 	next_command = input("> ")
+# print("Conv Over.")
+
+
+## TEST Embed with Additional Search
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+
+# First, let's load the language model we're going to use to control the agent.
+llm = OpenAI(temperature=0)
+
+# Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+tools = load_tools(["ddg-search", "llm-math"], llm=llm)
+
+
+# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+# Now let's test it out!
+agent.run("What was the high temperature in SF yesterday in Fahrenheit? What is that number raised to the .023 power?")
 
