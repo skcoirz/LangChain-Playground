0973fa195314ac562016a96686ae4761203c0d93
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-08 20:43:22 -0700

    temp changes

--- pandas_test.py
+++ pandas_test.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 df = pd.read_csv('titanic_train.csv')
 agent = create_pandas_dataframe_agent(
-    OpenAI(temperature=0.5), df, verbose=True
+    OpenAI(temperature=0.5),
+    df,
+    verbose=True
 )
 
 # agent.run("how many rows are there")
