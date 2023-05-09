34ec993bca7cdec5adb06893c4d48a298822b652
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-08 21:50:19 -0700

    add another test

--- huggingface_llm.py
+++ huggingface_llm.py
@@ -31,6 +31,19 @@ def example_with_everything_local():
 	res = generate_text("Explain to me the difference between nuclear fission and fusion.")
 	print(res[0]["generated_text"])
 
-example_with_everything_local()
+# example_with_everything_local()
 # this takes forever...
 
+
+def example_not_trust_dolly_example():
+	import torch
+	from instruct_pipeline import InstructionTextGenerationPipeline
+	from transformers import AutoModelForCausalLM, AutoTokenizer
+
+	tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b", padding_side="left")
+	model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b", device_map="auto", torch_dtype=torch.float16)
+	generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
+	res = generate_text("Explain to me the difference between nuclear fission and fusion.")
+	print(res[0]["generated_text"])
+
+example_not_trust_dolly_example()
